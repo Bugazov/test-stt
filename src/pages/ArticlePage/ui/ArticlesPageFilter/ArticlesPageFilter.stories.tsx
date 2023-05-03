83cbb959e124dd7a2c5ab0ea057f956966642e1a
OdinTakoy <bugazov9@mail.ru>
2023-05-03 21:28:47 +0300

    реализовал переиспользуемый компонент shared/Tabs

--- src/pages/ArticlePage/ui/ArticlesPageFilter/ArticlesPageFilter.stories.tsx
+++ src/pages/ArticlePage/ui/ArticlesPageFilter/ArticlesPageFilter.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { ArticlesPageFilter } from './ArticlesPageFilter';
 
 export default {
-    title: 'shared/ArticlesPageFilter',
+    title: 'pages/ArticlesPageFilter',
     component: ArticlesPageFilter,
     argTypes: {
         backgroundColor: { control: 'color' },
